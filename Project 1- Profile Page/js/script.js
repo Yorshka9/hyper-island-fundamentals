5c191d06502bf527e8797f2fe812593f4e03b127
tjeldflaat <43675119+Yorshka9@users.noreply.github.com>
2019-09-04 01:45:08 +0200

    Add files via upload

--- Project 1- Profile Page/js/script.js
+++ Project 1- Profile Page/js/script.js
@@ -29,3 +29,13 @@ $('.hidden').addClass('unhide');
         });
         
     });
+
+
+    window.addEventListener('scroll', () => {
+        let parent = document.getElementById('parallax-container');
+        let children = parent.getElementsByTagName('div');
+        for(let i = 0; i < children.length; i++) {
+          children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
+        }
+     }, false)
+     
